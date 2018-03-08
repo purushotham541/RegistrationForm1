79292af2333a731ad9dde414f1404291df71a538
purushotham <purushotham541@gmail.com>
2018-03-08 21:26:13 +0530

    initial

---
+++ app/src/main/java/com/example/purushotham/registrationform1/Mydata.java
@@ -0,0 +1,27 @@
+package com.example.purushotham.registrationform1;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by purushotham on 3/8/2018.
+ */
+
+public class Mydata extends SQLiteOpenHelper
+{
+    public Mydata(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
